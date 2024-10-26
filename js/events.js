142c6d3cfe9b930d66647cf62dbf3d14e456486d
Master08s <hb111a@icloud.com>
2024-10-26 20:47:39 +0800

    Site updated: 2024-10-26 20:47:39

--- js/events.js
+++ js/events.js
@@ -75,14 +75,18 @@ Fluid.events = {
   },
 
   registerScrollTopArrowEvent: function() {
-    var topArrow = $('#scroll-top-button');
+    var topArrow = jQuery('#scroll-top-button');
     if (topArrow.length === 0) {
       return;
     }
-    var board = $('#board');
+    var board = jQuery('#board');
     if (board.length === 0) {
       return;
     }
+    var arrowUpIcon = jQuery('#scroll-top-button i');
+    if(arrowUpIcon.length === 0){
+      return;
+    } 
     var posDisplay = false;
     var scrollDisplay = false;
     // Position
@@ -92,24 +96,29 @@ Fluid.events = {
       var right = bodyWidth - boardRight;
       posDisplay = right >= 50;
       topArrow.css({
-        'bottom': posDisplay && scrollDisplay ? '20px' : '-60px',
-        'right' : right - 64 + 'px'
+        'bottom': scrollDisplay ? '20px' : '-60px',
+        'right' : posDisplay ? right - 64 : 8 + 'px',
+        'min-width' : posDisplay ? 40 : 28 + 'px',
+        'min-height' : posDisplay ? 40 : 28 + 'px'
+      });
+      arrowUpIcon.css({
+        'font-size' : posDisplay ? 32 : 20 + 'px'
       });
     };
     setTopArrowPos();
-    $(window).resize(setTopArrowPos);
+    jQuery(window).resize(setTopArrowPos);
     // Display
     var headerHeight = board.offset().top;
     Fluid.utils.listenScroll(function() {
       var scrollHeight = document.body.scrollTop + document.documentElement.scrollTop;
       scrollDisplay = scrollHeight >= headerHeight;
       topArrow.css({
-        'bottom': posDisplay && scrollDisplay ? '20px' : '-60px'
+        'bottom': scrollDisplay ? '20px' : '-60px'
       });
     });
     // Click
     topArrow.on('click', function() {
-      $('body,html').animate({
+      jQuery('body,html').animate({
         scrollTop: 0,
         easing   : 'swing'
       });
